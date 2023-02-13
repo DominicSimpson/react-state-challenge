4c5a03b7267b91b23f4d26567d14706aa90d37ec
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-13 12:58:27 +0000

    Second challenge attempted

--- challenge/ListDishes.jsx
+++ challenge/ListDishes.jsx
@@ -1,18 +1,15 @@
 import dishes from "./data.js";
 
    export default function ListDishes( { max, setMax }) {
-    function update(event) {
-      setMax(+event.target.value);
-  } 
     return (
     <section aria-label="dishes">
         <ul className="grid">
           {dishes.map((dish) => (
+            /* enter in filter functions here */
             <li key={dish.id} className="card">
               <h3>{dish.name}</h3>
               {dish.description && <p>{dish.description}</p>}
               <div>£{dish.price.toFixed(2)}</div>
-              dish.price={update}
             </li>
           ))}
         </ul>
